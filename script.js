4085668fd1d234b13336e86814424cce15c36951
Juancaceres963 <dnijuancaceres@gmail.com>
2023-01-29 18:19:14 -0300

    Navbar, navbar lateral, Home

---
+++ script.js
@@ -0,0 +1,8 @@
+// Animacion de menu 
+
+const icons = document.querySelectorAll('.icon');
+icons.forEach (icon => {  
+  icon.addEventListener('click', (event) => {
+    icon.classList.toggle("open");
+  });
+});
