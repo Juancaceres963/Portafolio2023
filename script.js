d7e567c5461fca44206907e0cb33ebe95724c3ec
Juancaceres963 <dnijuancaceres@gmail.com>
2023-01-31 19:52:19 -0300

    Section my work complete and somethings more

--- script.js
+++ script.js
@@ -1,8 +1,28 @@
 // Animacion de menu 
 
-const icons = document.querySelectorAll('.icon');
+let icons = document.querySelectorAll('.icon');
 icons.forEach (icon => {  
   icon.addEventListener('click', (event) => {
     icon.classList.toggle("open");
   });
 });
+
+/*
+function bgAnimation () {
+  document.getElementById("bg-nav").classList.remove("container-menu");
+}
+
+function navToggle () {
+  document.getElementById("burguer-nav").classList.remove("cont-menu")
+}
+
+function burguerNav () {
+  document.getElementById("burguer-label").classList.remove("open")
+}
+
+document.getElementById("nav-link-desple").onclick = function () {
+  navToggle();
+  bgAnimation();
+  burguerNav();
+}
+*/
